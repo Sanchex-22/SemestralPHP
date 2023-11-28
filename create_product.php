18efa37d7436454350d018029efae935a8ddd018
Sanchex-22 <David21.cs70@gmail.com>
2023-11-28 11:34:08 -0500

    delete

--- create_product.php
+++ create_product.php
@@ -1,11 +1,47 @@
+<?php
+session_start();
+if (!isset($_SESSION["user_name"])) {
+    header("Location: index.php");
+    exit();
+}
+$username = $_SESSION["user_name"];
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Crear</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script>
+      tailwind.config = {
+        theme: {
+          extend: {
+            colors: {
+              violet1: '#B0578D',
+              violet2: '#D988B9',
+              violet3: '#FACBEA',
+              violet4: '#FFE4D6',
+            }
+          }
+        }
+      }
+    </script>
 </head>
 <body>
-    
+    <div class="flex flex-col h-screen" >
+        <nav class="w-full flex justify-between align-center p-5 bg-violet2">
+        <img src="#" alt="#">
+        <ul class="flex">
+            <li class="ml-5"><a href="dashboard.php">Productos</a></li>
+            <li class="ml-5"><a href="users_list.php">Usuarios</a></li>
+            <li class="ml-5">Info</li>
+            <li class="ml-5"><?php echo $_SESSION["user_name"]?></li>
+            <form class="ml-5 mr-5" action="logout.php" method="post">
+            <button type="submit" value="Logout" class="btn-logout">Logout</button>
+        </form>
+        </ul>
+        </nav>
+    </div>
 </body>
 </html>
--- products_services.js
+++ products_services.js
@@ -19,35 +19,18 @@ export class productsServices{
 
     }
 
-    deleteProducts(){
-        document.addEventListener('DOMContentLoaded', function() {
-            // Asegúrate de que el elemento 'eliminate' exista
-            const eliminateForm = document.getElementById('eliminate');
-
-            if (eliminateForm) {
-                eliminateForm.addEventListener('submit', function(event) {
-                    event.preventDefault();
-                    const cod = document.getElementById('cod').value;
-                    console.log(cod);
-                    const formData = {
-                        cod: cod,
-                    };
-
-                    fetch('api/delete.php', {
-                        method: 'POST',
-                        body: JSON.stringify(formData),
-                    })
-                    .then(response => response.text())
-                    .then(data => {
-                        alert("Tarea eliminada exitosamente: " + data);
-                    })
-                    .catch(error => {
-                        console.error('Error:', error);
-                    });
-                });
-            } else {
-                console.error("Elemento 'eliminate' no encontrado");
-            }
+    deleteProducts(formData){
+        console.log(formData)
+        fetch('http://localhost/SemestralPHP/api/products/delete.php', {
+            method: 'POST',
+            body: JSON.stringify(formData),
+        })
+        .then(response => response.text())
+        .then(data => {
+            alert("Tarea eliminada exitosamente: " + data);
+        })
+        .catch(error => {
+            console.error('Error:', error);
         });
     }
 }
